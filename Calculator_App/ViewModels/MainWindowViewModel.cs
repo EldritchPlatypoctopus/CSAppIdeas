6e4d84d9446ec5e8c8c673e1129f6c5369bd1a93
EldritchPlatypoctopus <tamsingillie@gmail.com>
2024-08-17 22:33:39 -0700

    feat(calc): initial UI for integer calculator

--- Calculator_App/ViewModels/MainWindowViewModel.cs
+++ Calculator_App/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,14 @@
-﻿namespace Calculator_App.ViewModels;
+﻿using System;
+
+namespace Calculator_App.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private int _shownValue = 0;
+    public int ShownValue { get; }
+
+    public void Exit()
+    {
+        Environment.Exit(0);
+    }
 }
---
+++ Calculator_App/Views/IntegerCalculator.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Calculator_App.Views;
+
+public partial class IntegerCalculator : UserControl
+{
+    public IntegerCalculator()
+    {
+        InitializeComponent();
+    }
+}
